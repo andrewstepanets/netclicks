2818e2c21d2b4dd76d11d52a99347b24c68ab663
Andrii Stepanets <ovrass@gmail.com>
2020-05-27 19:03:07 +0300

    finished left menu

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,32 @@ const burgerMenu = document.querySelector('.hamburger');
 
 // open/close burger menu
 
-function handleBurgerMenu(event){
-    leftMenu.classList.toggle('openMenu');
-    burgerMenu.classList.toggle('open'); 
+function handleBurgerMenu(){
+        leftMenu.classList.toggle('openMenu');
+        burgerMenu.classList.toggle('open');
+}
+
+// close menu if click outside
+
+function handleClickOutsideMenu(event) {
+    if(!event.target.closest('.left-menu')) {
+        leftMenu.classList.remove('openMenu');
+        burgerMenu.classList.remove('open');
+    }
+}
+
+function handleSubMenu(event){
+    const dropdown = event.target.closest('.dropdown');
+    if (dropdown) {
+        dropdown.classList.toggle('active');
+        leftMenu.classList.add('openMenu');
+        burgerMenu.classList.add('open');
+    }
+    
 }
 
 burgerMenu.addEventListener('click', handleBurgerMenu);
+
+document.addEventListener('click', handleClickOutsideMenu);
+
+leftMenu.addEventListener('click', handleSubMenu);
